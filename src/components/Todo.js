907f90286f2cf27da68badf433ed65fddd867452
dkcvarma <dkchaitanyavarma06@gmail.com>
2019-07-13 11:17:33 +0530

    Reverting back to 2 states

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
 
 const todo = props => {
-    // const [todoName, setTodoName] = useState('');
-    // const [todoList, SetTodoList] = useState([]);
-
-    const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
+    const [todoName, setTodoName] = useState('');
+    const [todoList, SetTodoList] = useState([]);
+    // const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
     const inputChangeHandler = (event) => {
-        setTodoState({
-            userInput: event.target.value,
-            todoList: todoState.todoList
-        });
+        // setTodoState({
+        //     userInput: event.target.value,
+        //     todoList: todoState.todoList
+        // });
+        setTodoName(event.target.value);
     }
 
     const todoAddHandler = () => {
-        setTodoState({
-            userInput: todoState.userInput,
-            todoList: todoState.todoList.concat(todoState.userInput)
-        });
+        // setTodoState({
+        //     userInput: todoState.userInput,
+        //     todoList: todoState.todoList.concat(todoState.userInput)
+        // });
+        SetTodoList(todoList.concat(todoName));
     }
 
     return (
@@ -26,11 +27,11 @@ const todo = props => {
                 type="text" 
                 placeholder="Todo" 
                 onChange={inputChangeHandler} 
-                value={todoState.userInput} 
+                value={todoName} 
             />
             <button type="button" onClick={todoAddHandler}>Add</button>
             <ul>
-                {todoState.todoList.map(todo => (
+                {todoList.map(todo => (
                     <li key={todo}>{todo}</li>
                 ))}
             </ul>
